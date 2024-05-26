5d94552e08e1b8d13497038c18ac300d8607c59b
MichaelBrunner <brunner@cadwork.swiss>
2024-05-26 19:32:19 +0200

    ENH: initial commit

---
+++ gtfs/src/main.cpp
@@ -0,0 +1,17 @@
+﻿// raptorxx.cpp : Defines the entry point for the application.
+//
+
+#include <iostream>
+#include "spdlog/spdlog.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
+
+
+int main() {
+  std::cout << "Hello CMake." << std::endl;
+
+  auto console = spdlog::stdout_color_mt("console");
+  spdlog::info("Welcome to spdlog version {}.{}.{}  !", SPDLOG_VER_MAJOR, SPDLOG_VER_MINOR, SPDLOG_VER_PATCH);
+  spdlog::warn("This is a warning message {}", 1234);
+
+  return 0;
+}
